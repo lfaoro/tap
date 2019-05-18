a23d3aef4fb5a79dc066c6fdbc0922105e8f9dca
Leonardo Faoro <lfaoro@gmail.com>
2019-05-18 13:05:07 +0200

    Brew formula update for ncrypt version v1.0.2

--- ncrypt.rb
+++ ncrypt.rb
@@ -2,9 +2,9 @@
 class Ncrypt < Formula
   desc "a geeky & friendly way to simply encrypt locally & share."
   homepage "https://github.com/lfaoro"
-  url "https://github.com/lfaoro/ncrypt/releases/download/v1.0.1/ncrypt_1.0.1_darwin_amd64.tar.gz"
-  version "1.0.1"
-  sha256 "6e07c847440dfd238b206656854edced479d0f0985a75489108a1f66a487e621"
+  url "https://github.com/lfaoro/ncrypt/releases/download/v1.0.2/ncrypt_1.0.2_darwin_amd64.tar.gz"
+  version "1.0.2"
+  sha256 "c8e2c46e64f4cec21f8779c4197666cd0e905cf43424de8b1844a979234767bc"
 
   def install
     bin.install "ncrypt"
