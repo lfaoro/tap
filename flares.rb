42e7191e4143cda4239d8f265b87a2434a99c5e3
Leonardo Faoro <lfaoro@gmail.com>
2019-08-04 22:42:44 +0200

    Brew formula update for flares version 2.3.2

--- flares.rb
+++ flares.rb
@@ -2,9 +2,16 @@
 class Flares < Formula
   desc "Flares is a CloudFlare DNS backup tool"
   homepage "https://github.com/lfaoro/flares"
-  url "https://github.com/lfaoro/flares/releases/download/v2.3.1/flares_2.3.1_darwin_amd64.tar.gz"
-  version "2.3.1"
-  sha256 "83b10e5eb60a3651e031bbe091c8d1083d3f4ef3d6eca3d2a1b10e3d7222d27d"
+  version "2.3.2"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/lfaoro/flares/releases/download/2.3.2/flares_2.3.2_darwin_amd64.tar.gz"
+    sha256 "ca09eb9c2bceb4620bf5acd00c461dad810d82d8d4525910483aeadeef5e1014"
+  elsif OS.linux?
+    url "https://github.com/lfaoro/flares/releases/download/2.3.2/flares_2.3.2_linux_amd64.tar.gz"
+    sha256 "27e88c5114b70088da7e49288514a71fe784e8391bdd6b5ecd1f8cdf8a044805"
+  end
 
   def install
     bin.install "flares"
